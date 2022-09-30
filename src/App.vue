5743d0abe7531902173679c7973d648aa155ac9d
Judit Bancells Vélez <judit@juditbancells.cat>
2022-09-30 09:26:23 +0200

    setup router

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,7 @@
 </script>
 
 <template>
-	<header>
-		<img
-			alt="Vue logo"
-			class="logo"
-			src="@/assets/logo.svg"
-			width="125"
-			height="125"
-		/>
-
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-			</nav>
-		</div>
-	</header>
-
+	<div class="app"></div>
 	<RouterView />
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/"),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "root",
+      component: () =>
+        import(/* webpackChunkName: 'index' */ "./pages/index.vue"),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/list",
+      name: "list",
+      component: () =>
+        import(/* webpackChunkName: 'list' */ "./pages/CountriesList.vue"),
+      children: [
+        {
+          path: "/details",
+          name: "details",
+          component: () =>
+            import(
+              /* webpackChunkName: 'details' */ "./pages/CountriesDetails.vue"
+            ),
+        },
+      ],
+    },
+  ],
+  scrollBehavior() {
+    document.getElementById("app").scrollIntoView();
+  },
+});
 
-export default router
+export default router;
